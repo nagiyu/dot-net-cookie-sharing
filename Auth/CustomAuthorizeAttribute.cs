2cb3e622341ac6a2fddf0491d02c6ba0b196e3b8
nagiyu <otonagiyu@gmail.com>
2024-08-21 21:52:45 +0900

    chore: Add Auth project to solution

---
+++ Auth/CustomAuthorizeAttribute.cs
@@ -0,0 +1,20 @@
+﻿#if NET48
+using System.Web.Mvc;
+#endif
+
+namespace Auth
+{
+#if NET48
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+#elif NET8_0
+    public class CustomAuthorizeAttribute
+#endif
+    {
+#if NET48
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+#endif
+    }
+}
--- Web.Framework/Controllers/PrivateController.cs
+++ Web.Framework/Controllers/PrivateController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Auth;
+
 namespace Web.Framework.Controllers
 {
-    [Authorize]
+    [CustomAuthorize]
     public class PrivateController : Controller
     {
         // GET: Private
