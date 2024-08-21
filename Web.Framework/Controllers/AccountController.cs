d3ec439fe60d83abdf44f07cdcd7fd59809f765c
nagiyu <otonagiyu@gmail.com>
2024-08-21 21:10:10 +0900

    feat: Add login functionality to AccountController
    This commit adds the necessary code to the AccountController to handle user login. It includes the implementation of the Login action method for both GET and POST requests, as well as the Logout action method. The Login view is also added to the Views/Account folder.
    
    The changes in this commit allow users to enter their username and log in to the application. If the username is empty, an error message is displayed. If the user is already authenticated, the navigation bar shows the user's name and a logout button. If the user is not authenticated, the navigation bar shows a login button.
    
    Note: This commit message follows the conventional format of "feat: <description>".

---
+++ Web.Framework/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Web.Framework.Controllers
+{
+    public class AccountController : Controller
+    {
+        // ログインフォームを表示
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // ログインフォームからPOSTされたデータを受け取る
+        [HttpPost]
+        public ActionResult Login(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                // ユーザー名でログインする（パスワードは省略）
+                FormsAuthentication.RedirectFromLoginPage(username, false);
+
+                // この後、指定したリダイレクト先に転送される
+            }
+            else
+            {
+                // ユーザー名が空の場合のエラー処理
+                ModelState.AddModelError("", "ユーザー名を入力してください。");
+                return View();
+            }
+
+            // ここには到達しない
+            return null;
+        }
+
+        // ログアウト処理
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
