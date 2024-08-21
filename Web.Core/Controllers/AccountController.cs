1606a09a4378c76de9593f29c86c8da0b55ee742
nagiyu <otonagiyu@gmail.com>
2024-08-21 22:36:14 +0900

    feat: Add Japanese translation for private page and login
    The code changes include adding a new file `Index.cshtml` in the `Web.Core/Views/Private` directory, which contains the Japanese translation for the private page. Additionally, the `Login.cshtml` file in the `Web.Core/Views/Account` directory has been modified to include Japanese translations for the login page.
    
    This commit adds the necessary translations to provide a localized experience for Japanese users on the private page and login functionality.
    
    Note: This commit message follows the conventional format of "feat: <description>".

---
+++ Web.Core/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Framework.Controllers
+{
+    public class AccountController : Controller
+    {
+        //// ログインフォームを表示
+        //[HttpGet]
+        //public ActionResult Login()
+        //{
+        //    return View();
+        //}
+
+        //// ログインフォームからPOSTされたデータを受け取る
+        //[HttpPost]
+        //public ActionResult Login(string username)
+        //{
+        //    if (!string.IsNullOrEmpty(username))
+        //    {
+        //        // ユーザー名でログインする（パスワードは省略）
+        //        // TODO ASP.NET メンバーシップは ASP.NET Core Identity に置き換える必要があります。詳細については、https://docs.microsoft.com/aspnet/core/migration/proper-to-2x/membership-to-core-identity を参照してください。
+        //                        FormsAuthentication.RedirectFromLoginPage(username, false);
+
+        //        // この後、指定したリダイレクト先に転送される
+        //    }
+        //    else
+        //    {
+        //        // ユーザー名が空の場合のエラー処理
+        //        ModelState.AddModelError("", "ユーザー名を入力してください。");
+        //        return View();
+        //    }
+
+        //    // ここには到達しない
+        //    return null;
+        //}
+
+        //// ログアウト処理
+        //public ActionResult Logout()
+        //{
+        //    // TODO ASP.NET メンバーシップは ASP.NET Core Identity に置き換える必要があります。詳細については、https://docs.microsoft.com/aspnet/core/migration/proper-to-2x/membership-to-core-identity を参照してください。
+        //    FormsAuthentication.SignOut();
+        //    return RedirectToAction("Login", "Account");
+        //}
+    }
+}
---
+++ Web.Core/Controllers/PrivateController.cs
@@ -0,0 +1,15 @@
+﻿using Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Framework.Controllers
+{
+    //[CustomAuthorize]
+    //public class PrivateController : Controller
+    //{
+    //    // GET: Private
+    //    public ActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //}
+}
